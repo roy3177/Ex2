f06a13fb07e26abda13b18729d75c7e4655793f5
boaz ben-moshe <benmo@g.ariel.ac.il>
2023-03-28 15:43:53 +0300

    Initial commit

---
+++ src/classes/week6/QuickSortTest.java
@@ -0,0 +1,18 @@
+package classes.week6;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class QuickSortTest {
+	@Test
+	void testSort() {
+		double[] arr = {1,-3.2,1,5,-3.2};
+		QuickSort.sort(arr);
+		boolean b = MyArrayLibrary.isSortedAscending(arr);
+		if(b!=true) {
+			fail("ERR: the array should be sorted!");
+		}
+	}
+
+}
